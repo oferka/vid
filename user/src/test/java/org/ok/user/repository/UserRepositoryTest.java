ba9bab3104045b052ba06bb255ad5a8589dbe304
ofer.karp <ofer.karp@gmail.com>
2022-01-05 18:15:13 +0200

    user module

--- user/src/test/java/org/ok/user/repository/UserRepositoryTest.java
+++ user/src/test/java/org/ok/user/repository/UserRepositoryTest.java
@@ -4,7 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.ok.user.UserTest;
 import org.ok.user.model.User;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.ok.TestDataUtils.getNonExistingId;
 
 public class UserRepositoryTest extends UserTest {
 
@@ -16,31 +20,31 @@ public class UserRepositoryTest extends UserTest {
         userRepository.delete(saved);
     }
 
-//    @Test
-//    void shouldSaveItems() {
-//        List<Account> items = contentProvider.get(getNumberOfItemsToLoad());
-//        Iterable<Account> saved = accountRepository.saveAll(items);
-//        assertNotNull(saved);
-//        accountRepository.deleteAll(saved);
-//    }
-//
-//    @Test
-//    void shouldFindItemById() {
-//        Account item = contentProvider.get();
-//        Account saved = accountRepository.save(item);
-//        Optional<Account> foundItemOptional = accountRepository.findById(item.getId());
-//        assertTrue(foundItemOptional.isPresent());
-//        Account foundItem = foundItemOptional.get();
-//        assertEquals(item.getId(), foundItem.getId());
-//        accountRepository.delete(saved);
-//    }
-//
-//    @Test
-//    void shouldNotFindItemById() {
-//        Optional<Account> foundItemOptional = accountRepository.findById(getNonExistingId());
-//        assertTrue(foundItemOptional.isEmpty());
-//    }
-//
+    @Test
+    void shouldSaveItems() {
+        List<User> items = contentProvider.get(getNumberOfItemsToLoad());
+        Iterable<User> saved = userRepository.saveAll(items);
+        assertNotNull(saved);
+        userRepository.deleteAll(saved);
+    }
+
+    @Test
+    void shouldFindItemById() {
+        User item = contentProvider.get();
+        User saved = userRepository.save(item);
+        Optional<User> foundItemOptional = userRepository.findById(item.getId());
+        assertTrue(foundItemOptional.isPresent());
+        User foundItem = foundItemOptional.get();
+        assertEquals(item.getId(), foundItem.getId());
+        userRepository.delete(saved);
+    }
+
+    @Test
+    void shouldNotFindItemById() {
+        Optional<User> foundItemOptional = userRepository.findById(getNonExistingId());
+        assertTrue(foundItemOptional.isEmpty());
+    }
+
 //    @Test
 //    void shouldFindItemBySymbol() {
 //        Account item = contentProvider.get();
