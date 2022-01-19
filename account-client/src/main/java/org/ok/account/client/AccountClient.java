c50db1c97a5b32d156c4f41a4e1b4e021787132e
ofer.karp <ofer.karp@gmail.com>
2022-01-19 16:17:59 +0200

    feign

--- account-client/src/main/java/org/ok/account/client/AccountClient.java
+++ account-client/src/main/java/org/ok/account/client/AccountClient.java
@@ -8,9 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
+import static org.ok.integration.Paths.ACCOUNT_PATH;
 import static org.ok.integration.Paths.RANDOM_ID_PATH;
+import static org.ok.integration.ServiceNames.ACCOUNT_SERVICE_NAME;
 
-@FeignClient(value = "accountClient", url = "http://localhost:54508/api/rest/account")
+@FeignClient(value = ACCOUNT_SERVICE_NAME, path = ACCOUNT_PATH)
 public interface AccountClient {
 
     @GetMapping
--- user/src/main/java/org/ok/user/UserApplication.java
+++ user/src/main/java/org/ok/user/UserApplication.java
@@ -5,12 +5,14 @@ import org.ok.user.data.content.loader.ContentLoader;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableFeignClients(basePackageClasses = { AccountClient.class })
 public class UserApplication {
 
