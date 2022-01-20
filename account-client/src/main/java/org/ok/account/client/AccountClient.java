ae5a23810f591422aaa7a83ee6e5bf0427f91065
ofer.karp <ofer.karp@gmail.com>
2022-01-20 08:07:19 +0200

    feign

--- account-client/src/main/java/org/ok/account/client/AccountClient.java
+++ account-client/src/main/java/org/ok/account/client/AccountClient.java
@@ -6,12 +6,14 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
-import static org.ok.integration.Paths.ACCOUNT_PATH;
-import static org.ok.integration.Paths.RANDOM_ID_PATH;
+import static org.ok.integration.Paths.*;
 import static org.ok.integration.ServiceNames.ACCOUNT_SERVICE_NAME;
 
 @FeignClient(value = ACCOUNT_SERVICE_NAME, path = ACCOUNT_PATH)
@@ -25,4 +27,10 @@ public interface AccountClient {
 
     @GetMapping(path = "{id}")
     @NotNull ResponseEntity<Account> findById(@PathVariable("id") @NotNull Long id);
+
+    @GetMapping(path = RANDOM_PATH)
+    @NotNull ResponseEntity<Account> findRandom();
+
+    @PostMapping
+    @NotNull ResponseEntity<Account> save(@RequestBody @NotNull Account account);
 }
