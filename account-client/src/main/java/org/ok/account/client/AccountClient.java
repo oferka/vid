c0c68551e3d0fb7ba34375353063b28c3cdbc9ba
ofer.karp <ofer.karp@gmail.com>
2022-01-20 07:58:48 +0200

    feign

--- account-client/src/main/java/org/ok/account/client/AccountClient.java
+++ account-client/src/main/java/org/ok/account/client/AccountClient.java
@@ -1,9 +1,11 @@
 package org.ok.account.client;
 
+import feign.Param;
 import org.ok.account.model.Account;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.validation.constraints.NotNull;
 import java.util.List;
@@ -20,4 +22,7 @@ public interface AccountClient {
 
     @GetMapping(path = RANDOM_ID_PATH)
     @NotNull ResponseEntity<Long> findRandomId();
+
+    @GetMapping(path = "{id}")
+    @NotNull ResponseEntity<Account> findById(@PathVariable("id") @NotNull Long id);
 }
