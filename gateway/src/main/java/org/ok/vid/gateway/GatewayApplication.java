077716adb586d4a43a629455dcd8881b9a0a126c
ofer.karp <ofer.karp@gmail.com>
2022-01-22 09:56:31 +0200

    model version maven

--- gateway/src/main/java/org/ok/vid/gateway/GatewayApplication.java
+++ gateway/src/main/java/org/ok/vid/gateway/GatewayApplication.java
@@ -3,6 +3,9 @@ package org.ok.vid.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,5 +13,17 @@ public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("account", r -> r
+                        .path("/api/rest/account/**")
+                        .uri("lb://account/api/rest/account"))
+                .route("user", r -> r
+                        .path("/api/rest/user/**")
+                        .uri("lb://user/api/rest/user"))
+                .build();
     }
 }
