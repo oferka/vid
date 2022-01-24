408b60f61c940fd73890bc424dfbdec1c0114582
ofer.karp <ofer.karp@gmail.com>
2022-01-24 18:33:23 +0200

    RoutesConfiguration

--- gateway/src/main/java/org/ok/vid/gateway/GatewayApplication.java
+++ gateway/src/main/java/org/ok/vid/gateway/GatewayApplication.java
@@ -3,14 +3,6 @@ package org.ok.vid.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
-
-import static org.ok.vid.integration.Paths.ACCOUNT_PATH;
-import static org.ok.vid.integration.Paths.USER_PATH;
-import static org.ok.vid.integration.ServiceNames.ACCOUNT_SERVICE_NAME;
-import static org.ok.vid.integration.ServiceNames.USER_SERVICE_NAME;
 
 @SpringBootApplication
 @EnableDiscoveryClient
