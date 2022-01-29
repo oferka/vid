9fed0f8d78faf38338afc228c22487ebb0394c4e
ofer.karp <ofer.karp@gmail.com>
2022-01-29 23:12:56 +0200

    micrometer

--- account/src/main/java/org/ok/vid/account/controller/AccountController.java
+++ account/src/main/java/org/ok/vid/account/controller/AccountController.java
@@ -35,8 +35,8 @@ public class AccountController {
     private final AccountService accountService;
 
     @GetMapping
-    @Timed(value = "AccountController.findAll.timer", description = "Timer for account findAll endpoint", extraTags = {"tag1.key", "tag1.value", "tag2.key", "tag2.value"}, percentiles = { 0.05,0.50, 0.95})
-    @Counted(value = "AccountController.findAll.counter", description = "Counter for account findAll endpoint", extraTags = {"tag1.key", "tag1.value", "tag2.key", "tag2.value"})
+    @Timed(value = "AccountController.findAll.timer", description = "Timer for account findAll endpoint", percentiles = { 0.01, 0.05,0.50, 0.95, 0.99})
+    @Counted(value = "AccountController.findAll.counter", description = "Counter for account findAll endpoint")
     @Operation(summary = "Find all accounts")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Accounts successfully found", content = { @Content(mediaType = APPLICATION_JSON_VALUE, array = @ArraySchema(schema = @Schema(implementation = Account.class)))}),
@@ -47,44 +47,52 @@ public class AccountController {
         return ResponseEntity.ok(items);
     }
 
+    @GetMapping(value = "{id}")
+    @Timed(value = "AccountController.findById.timer", description = "Timer for account findById endpoint", percentiles = { 0.01, 0.05,0.50, 0.95, 0.99})
+    @Counted(value = "AccountController.findById.counter", description = "Counter for account findById endpoint")
     @Operation(summary = "Find an account by id")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Account successfully found by id", content = { @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = Account.class))}),
             @ApiResponse(responseCode = "404", description = "Account with specified id was not found", content = @Content),
             @ApiResponse(responseCode = "400", description = "Failed to find account by id", content = @Content) })
-    @GetMapping(value = "{id}")
     public @NotNull ResponseEntity<Account> findById(@Parameter(description = "The id of the account to be found") @PathVariable("id") @NotNull Long id) {
         Optional<Account> item = accountService.findById(id);
         return item.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @GetMapping(path = RANDOM_PATH)
+    @Timed(value = "AccountController.findRandom.timer", description = "Timer for account findRandom endpoint", percentiles = { 0.01, 0.05,0.50, 0.95, 0.99})
+    @Counted(value = "AccountController.findRandom.counter", description = "Counter for account findRandom endpoint")
     @Operation(summary = "Find a random account")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Random account successfully found", content = { @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = Account.class))}),
             @ApiResponse(responseCode = "404", description = "Random account was not found", content = @Content),
             @ApiResponse(responseCode = "400", description = "Failed to find a random account", content = @Content) })
-    @GetMapping(path = RANDOM_PATH)
     public @NotNull ResponseEntity<Account> findRandom() {
         Optional<Account> item = accountService.findRandom();
         return item.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @GetMapping(path = RANDOM_ID_PATH)
+    @Timed(value = "AccountController.findRandomId.timer", description = "Timer for account findRandomId endpoint", percentiles = { 0.01, 0.05,0.50, 0.95, 0.99})
+    @Counted(value = "AccountController.findRandomId.counter", description = "Counter for account findRandomId endpoint")
     @Operation(summary = "Find a random account ID")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Random account ID successfully found", content = { @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = Long.class))}),
             @ApiResponse(responseCode = "404", description = "Random account ID was not found", content = @Content),
             @ApiResponse(responseCode = "400", description = "Failed to find a random account ID", content = @Content) })
-    @GetMapping(path = RANDOM_ID_PATH)
     public @NotNull ResponseEntity<Long> findRandomId() {
         Optional<Long> item = accountService.findRandomId();
         return item.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @PostMapping
+    @Timed(value = "AccountController.save.timer", description = "Timer for account save endpoint", percentiles = { 0.01, 0.05,0.50, 0.95, 0.99})
+    @Counted(value = "AccountController.save.counter", description = "Counter for account save endpoint")
     @Operation(summary = "Create an account")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "Account created successfully", content = { @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = Account.class))}),
             @ApiResponse(responseCode = "400", description = "Failed to create an account", content = @Content) })
-    @PostMapping
     public @NotNull ResponseEntity<Account> save(@Parameter(description = "Account to be saved") @RequestBody @Valid @NotNull Account account) {
         HttpHeaders httpHeaders = new HttpHeaders();
         URI location = linkTo(AccountController.class).slash(account.getId()).toUri();
@@ -93,42 +101,50 @@ public class AccountController {
         return new ResponseEntity<>(saved, httpHeaders, HttpStatus.CREATED);
     }
 
+    @DeleteMapping()
+    @Timed(value = "AccountController.delete.timer", description = "Timer for account delete endpoint", percentiles = { 0.01, 0.05,0.50, 0.95, 0.99})
+    @Counted(value = "AccountController.delete.counter", description = "Counter for account delete endpoint")
     @Operation(summary = "Delete an account")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "204", description = "Account successfully deleted"),
             @ApiResponse(responseCode = "400", description = "Failed to delete account", content = @Content) })
-    @DeleteMapping()
     public @NotNull ResponseEntity<Account> delete(@Parameter(description = "Account to be deleted") @RequestBody @NotNull @Valid Account account) {
         Optional<Account> deleted = accountService.delete(account);
         return deleted.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @DeleteMapping("/{id}")
+    @Timed(value = "AccountController.deleteById.timer", description = "Timer for account deleteById endpoint", percentiles = { 0.01, 0.05,0.50, 0.95, 0.99})
+    @Counted(value = "AccountController.deleteById.counter", description = "Counter for account deleteById endpoint")
     @Operation(summary = "Delete an account by id")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "204", description = "Account successfully deleted by id"),
             @ApiResponse(responseCode = "400", description = "Failed to delete account by id", content = @Content) })
-    @DeleteMapping("/{id}")
     public @NotNull ResponseEntity<Account> deleteById(@Parameter(description = "The id of the account to be deleted") @PathVariable("id") @NotNull Long id) {
         Optional<Account> deleted = accountService.deleteById(id);
         return deleted.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @PutMapping
+    @Timed(value = "AccountController.update.timer", description = "Timer for account update endpoint", percentiles = { 0.01, 0.05,0.50, 0.95, 0.99})
+    @Counted(value = "AccountController.update.counter", description = "Counter for account update endpoint")
     @Operation(summary = "Update an account")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Account updated successfully", content = { @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = Account.class))}),
             @ApiResponse(responseCode = "404", description = "Account not found", content = @Content),
             @ApiResponse(responseCode = "400", description = "Failed to update account", content = @Content) })
-    @PutMapping
     public @NotNull ResponseEntity<Account> update(@Parameter(description = "Account to be updated") @RequestBody @NotNull @Valid Account account) {
         Optional<Account> updated = accountService.update(account);
         return updated.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @GetMapping(path = COUNT_PATH)
+    @Timed(value = "AccountController.count.timer", description = "Timer for account count endpoint", percentiles = { 0.01, 0.05,0.50, 0.95, 0.99})
+    @Counted(value = "AccountController.count.counter", description = "Counter for account count endpoint")
     @Operation(summary = "Return the number of existing accounts")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Accounts counted successfully", content = { @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = Long.class))}),
             @ApiResponse(responseCode = "400", description = "Failed to count accounts", content = @Content) })
-    @GetMapping(path = COUNT_PATH)
     public @NotNull ResponseEntity<Long> count() {
         long count = accountService.count();
         return ResponseEntity.ok(count);
--- user/src/main/java/org/ok/vid/user/controller/UserController.java
+++ user/src/main/java/org/ok/vid/user/controller/UserController.java
@@ -35,8 +35,8 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping
-    @Timed(value = "UserController.findAll.timer", description = "Timer for user findAll endpoint", extraTags = {"tag1.key", "tag1.value", "tag2.key", "tag2.value"}, percentiles = { 0.05,0.50, 0.95})
-    @Counted(value = "UserController.findAll.counter", description = "Counter for user findAll endpoint", extraTags = {"tag1.key", "tag1.value", "tag2.key", "tag2.value"})
+    @Timed(value = "UserController.findAll.timer", description = "Timer for user findAll endpoint", percentiles = { 0.01, 0.05,0.50, 0.95, 0.99})
+    @Counted(value = "UserController.findAll.counter", description = "Counter for user findAll endpoint")
     @Operation(summary = "Find all users")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Users successfully found", content = { @Content(mediaType = APPLICATION_JSON_VALUE, array = @ArraySchema(schema = @Schema(implementation = User.class)))}),
@@ -47,44 +47,52 @@ public class UserController {
         return ResponseEntity.ok(items);
     }
 
+    @GetMapping(value = "{id}")
+    @Timed(value = "UserController.findById.timer", description = "Timer for user findById endpoint", percentiles = { 0.01, 0.05,0.50, 0.95, 0.99})
+    @Counted(value = "UserController.findById.counter", description = "Counter for user findById endpoint")
     @Operation(summary = "Find a user by id")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "User successfully found by id", content = { @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = User.class))}),
             @ApiResponse(responseCode = "404", description = "User with specified id was not found", content = @Content),
             @ApiResponse(responseCode = "400", description = "Failed to find user by id", content = @Content) })
-    @GetMapping(value = "{id}")
     public @NotNull ResponseEntity<User> findById(@Parameter(description = "The id of the user to be found") @PathVariable("id") @NotNull Long id) {
         Optional<User> item = userService.findById(id);
         return item.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @GetMapping(path = RANDOM_PATH)
+    @Timed(value = "UserController.findRandom.timer", description = "Timer for user findRandom endpoint", percentiles = { 0.01, 0.05,0.50, 0.95, 0.99})
+    @Counted(value = "UserController.findRandom.counter", description = "Counter for user findRandom endpoint")
     @Operation(summary = "Find a random user")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Random user successfully found", content = { @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = User.class))}),
             @ApiResponse(responseCode = "404", description = "Random user was not found", content = @Content),
             @ApiResponse(responseCode = "400", description = "Failed to find a random user", content = @Content) })
-    @GetMapping(path = RANDOM_PATH)
     public @NotNull ResponseEntity<User> findRandom() {
         Optional<User> item = userService.findRandom();
         return item.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @GetMapping(path = RANDOM_ID_PATH)
+    @Timed(value = "UserController.findRandomId.timer", description = "Timer for user findRandomId endpoint", percentiles = { 0.01, 0.05,0.50, 0.95, 0.99})
+    @Counted(value = "UserController.findRandomId.counter", description = "Counter for user findRandomId endpoint")
     @Operation(summary = "Find a random user ID")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Random user ID successfully found", content = { @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = Long.class))}),
             @ApiResponse(responseCode = "404", description = "Random user ID was not found", content = @Content),
             @ApiResponse(responseCode = "400", description = "Failed to find a random user ID", content = @Content) })
-    @GetMapping(path = RANDOM_ID_PATH)
     public @NotNull ResponseEntity<Long> findRandomId() {
         Optional<Long> item = userService.findRandomId();
         return item.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @PostMapping
+    @Timed(value = "UserController.save.timer", description = "Timer for user save endpoint", percentiles = { 0.01, 0.05,0.50, 0.95, 0.99})
+    @Counted(value = "UserController.save.counter", description = "Counter for user save endpoint")
     @Operation(summary = "Create a user")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "User created successfully", content = { @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = User.class))}),
             @ApiResponse(responseCode = "400", description = "Failed to create an user", content = @Content) })
-    @PostMapping
     public @NotNull ResponseEntity<User> save(@Parameter(description = "User to be saved") @RequestBody @Valid @NotNull User user) {
         HttpHeaders httpHeaders = new HttpHeaders();
         URI location = linkTo(UserController.class).slash(user.getId()).toUri();
@@ -93,42 +101,50 @@ public class UserController {
         return new ResponseEntity<>(saved, httpHeaders, HttpStatus.CREATED);
     }
 
+    @DeleteMapping()
+    @Timed(value = "UserController.delete.timer", description = "Timer for user delete endpoint", percentiles = { 0.01, 0.05,0.50, 0.95, 0.99})
+    @Counted(value = "UserController.delete.counter", description = "Counter for user delete endpoint")
     @Operation(summary = "Delete a user")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "204", description = "User successfully deleted"),
             @ApiResponse(responseCode = "400", description = "Failed to delete user", content = @Content) })
-    @DeleteMapping()
     public @NotNull ResponseEntity<User> delete(@Parameter(description = "User to be deleted") @RequestBody @NotNull @Valid User user) {
         Optional<User> deleted = userService.delete(user);
         return deleted.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @DeleteMapping("/{id}")
+    @Timed(value = "UserController.deleteById.timer", description = "Timer for user deleteById endpoint", percentiles = { 0.01, 0.05,0.50, 0.95, 0.99})
+    @Counted(value = "UserController.deleteById.counter", description = "Counter for user deleteById endpoint")
     @Operation(summary = "Delete a user by id")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "204", description = "User successfully deleted by id"),
             @ApiResponse(responseCode = "400", description = "Failed to delete user by id", content = @Content) })
-    @DeleteMapping("/{id}")
     public @NotNull ResponseEntity<User> deleteById(@Parameter(description = "The id of the user to be deleted") @PathVariable("id") @NotNull Long id) {
         Optional<User> deleted = userService.deleteById(id);
         return deleted.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @PutMapping
+    @Timed(value = "UserController.update.timer", description = "Timer for user deleteById endpoint", percentiles = { 0.01, 0.05,0.50, 0.95, 0.99})
+    @Counted(value = "UserController.update.counter", description = "Counter for user update endpoint")
     @Operation(summary = "Update a user")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "User updated successfully", content = { @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = User.class))}),
             @ApiResponse(responseCode = "404", description = "User not found", content = @Content),
             @ApiResponse(responseCode = "400", description = "Failed to update user", content = @Content) })
-    @PutMapping
     public @NotNull ResponseEntity<User> update(@Parameter(description = "User to be updated") @RequestBody @NotNull @Valid User user) {
         Optional<User> updated = userService.update(user);
         return updated.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @GetMapping(path = COUNT_PATH)
+    @Timed(value = "UserController.count.timer", description = "Timer for user count endpoint", percentiles = { 0.01, 0.05,0.50, 0.95, 0.99})
+    @Counted(value = "UserController.count.counter", description = "Counter for user count endpoint")
     @Operation(summary = "Return the number of existing users")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Users counted successfully", content = { @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = Long.class))}),
             @ApiResponse(responseCode = "400", description = "Failed to count users", content = @Content) })
-    @GetMapping(path = COUNT_PATH)
     public @NotNull ResponseEntity<Long> count() {
         long count = userService.count();
         return ResponseEntity.ok(count);
