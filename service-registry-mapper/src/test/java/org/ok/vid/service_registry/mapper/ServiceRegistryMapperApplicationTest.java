fab8431588f9808f56396ee5bcaa2a1956183631
ofer.karp <ofer.karp@gmail.com>
2022-02-04 12:28:44 +0200

    service registry mapper

---
+++ service-registry-mapper/src/test/java/org/ok/vid/service_registry/mapper/ServiceRegistryMapperApplicationTest.java
@@ -0,0 +1,15 @@
+package org.ok.vid.service_registry.mapper;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+class ServiceRegistryMapperApplicationTest {
+
+    @Test
+    void contextLoads() {
+        log.info("Application loaded successfully");
+    }
+}
